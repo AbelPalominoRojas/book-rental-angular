aac20215e1f05c4241468f41303e70febb1548bf
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-04-28 19:28:20 -0500

    Editorial: route index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'editorial',
+    loadChildren: () => import('@/modules/editorial/editorial.module').then(m=>m.EditorialModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
