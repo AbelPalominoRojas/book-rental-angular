f134b91fbb20002130b8f1da942d29782a4a92a5
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-04-29 15:01:09 -0500

    Add enviroment files

--- src/app/services/editorial.service.ts
+++ src/app/services/editorial.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EditorialModel } from '@/models'
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,22 @@ export class EditorialService {
   ) { }
 
   getAllEditoriales(){
-    return this.http.get<EditorialModel[]>('http://localhost:8080/editoriales');
+    return this.http.get<EditorialModel[]>(`${environment.apiBaseUrl}/editoriales`);
   }
 
   getEditorialById(id: number){
-    return this.http.get<EditorialModel>(`http://localhost:8080/editoriales/${id}`);
+    return this.http.get<EditorialModel>(`${environment.apiBaseUrl}/editoriales/${id}`);
   }
 
   createEditorial(payload: EditorialModel){
-    return this.http.post<EditorialModel>('http://localhost:8080/editoriales',payload);
+    return this.http.post<EditorialModel>(`${environment.apiBaseUrl}/editoriales`,payload);
   }
 
   editEditorial(id: number,payload: EditorialModel){
-    return this.http.put<EditorialModel>(`http://localhost:8080/editoriales/${id}`, payload);
+    return this.http.put<EditorialModel>(`${environment.apiBaseUrl}/editoriales/${id}`, payload);
   }
 
   removeEditorialById(id: number){
-    return this.http.delete<EditorialModel>(`http://localhost:8080/editoriales/${id}`);
+    return this.http.delete<EditorialModel>(`${environment.apiBaseUrl}/editoriales/${id}`);
   }
 }
